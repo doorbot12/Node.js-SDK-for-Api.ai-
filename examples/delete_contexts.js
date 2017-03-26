7e6d56cc6cfa62104b33962533abe17fdf079226
doorbot12 <barubeno@gmail.com>
2017-03-27 04:17:54 +0700

    Update delete_contexts.js

--- examples/delete_contexts.js
+++ examples/delete_contexts.js
@@ -8,10 +8,10 @@
 
 var apiai = require("apiai");
 
-var app = apiai("YOUR_ACCESS_TOKEN");
+var app = apiai("wnSEoGyt8FGC6wpwEs5Mnb3xqeuKLWbtr1tNtEEiyTjf0cm/rIrjAff3JzZkMDY8Di87XAMMZlbGKHym/DvJxBMbTgfMx6Kvg9itSXDrmMhz4J1VUu5nA5y3xB4n1IcRHf4F3OLqe21tMeRuLs167QdB04t89/1O/w1cDnyilFU=");
 
 var options = {
-    sessionId: '<UNIQE SESSION ID>'
+    sessionId: ' 	1503638187'
 };
 
 var request = app.deleteContextsRequest(options);
