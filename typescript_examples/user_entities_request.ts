b0948101572af72e208832d2a7918fea282c2b6d
doorbot12 <barubeno@gmail.com>
2017-03-27 04:45:40 +0700

    Update user_entities_request.ts

--- typescript_examples/user_entities_request.ts
+++ typescript_examples/user_entities_request.ts
@@ -4,9 +4,9 @@
 
 import * as apiai from "../";
 
-const app = apiai("YOUR_ACCESS_TOKEN");
+const app = apiai("wnSEoGyt8FGC6wpwEs5Mnb3xqeuKLWbtr1tNtEEiyTjf0cm/rIrjAff3JzZkMDY8Di87XAMMZlbGKHym/DvJxBMbTgfMx6Kvg9itSXDrmMhz4J1VUu5nA5y3xB4n1IcRHf4F3OLqe21tMeRuLs167QdB04t89/1O/w1cDnyilFU=");
 
-const sessionId = "UNIQUE_SESSION_ID";
+const sessionId = " 	1503638187";
 
 let user_entities_request = app.userEntitiesRequest({
     sessionId: sessionId,
