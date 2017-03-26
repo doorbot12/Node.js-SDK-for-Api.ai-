b3e6b34fb8b57dc61729f027cb3b53e2eb11c5cb
doorbot12 <barubeno@gmail.com>
2017-03-27 04:35:58 +0700

    Update tts_request.js

--- examples/tts_request.js
+++ examples/tts_request.js
@@ -6,7 +6,7 @@
 var fs = require("fs");
 var apiai = require("apiai");
 
-var app = apiai("YOUR_ACCESS_TOKEN");
+var app = apiai("wnSEoGyt8FGC6wpwEs5Mnb3xqeuKLWbtr1tNtEEiyTjf0cm/rIrjAff3JzZkMDY8Di87XAMMZlbGKHym/DvJxBMbTgfMx6Kvg9itSXDrmMhz4J1VUu5nA5y3xB4n1IcRHf4F3OLqe21tMeRuLs167QdB04t89/1O/w1cDnyilFU=");
 
 
 var file = fs.createWriteStream('hello-world-tts.wav');
