aed38b8df86aaecccb231e316d539342d9e6bd60
doorbot12 <barubeno@gmail.com>
2017-03-27 04:29:10 +0700

    Update text_request_no_ssl.js

--- examples/text_request_no_ssl.js
+++ examples/text_request_no_ssl.js
@@ -15,10 +15,10 @@ var options = {
     endpoint: '/api/'
 };
 
-var app = apiai("YOUR_ACCESS_TOKEN", options);
+var app = apiai("wnSEoGyt8FGC6wpwEs5Mnb3xqeuKLWbtr1tNtEEiyTjf0cm/rIrjAff3JzZkMDY8Di87XAMMZlbGKHym/DvJxBMbTgfMx6Kvg9itSXDrmMhz4J1VUu5nA5y3xB4n1IcRHf4F3OLqe21tMeRuLs167QdB04t89/1O/w1cDnyilFU=", options);
 
 var options = {
-    sessionId: '<UNIQE SESSION ID>'
+    sessionId: '1503638187'
 };
 
 var request = app.textRequest('Hello', options);
