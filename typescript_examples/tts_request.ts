881be2b0c55a9f17ce503e039c20a440c3178298
doorbot12 <barubeno@gmail.com>
2017-03-27 04:43:50 +0700

    Update tts_request.ts

--- typescript_examples/tts_request.ts
+++ typescript_examples/tts_request.ts
@@ -6,7 +6,7 @@
 import * as apiai from "../";
 import * as fs from "fs";
 
-const app = apiai("YOUR_ACCESS_TOKEN");
+const app = apiai("wnSEoGyt8FGC6wpwEs5Mnb3xqeuKLWbtr1tNtEEiyTjf0cm/rIrjAff3JzZkMDY8Di87XAMMZlbGKHym/DvJxBMbTgfMx6Kvg9itSXDrmMhz4J1VUu5nA5y3xB4n1IcRHf4F3OLqe21tMeRuLs167QdB04t89/1O/w1cDnyilFU=");
 
 let file = fs.createWriteStream("hello-world-tts.wav");
 
